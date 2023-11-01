db04f3d3eb1424abdee55bea65fd75947550ec34
chrjl <73675826+chrjl@users.noreply.github.com>
2023-11-01 16:47:16 -0700

    feat(contentlayer,Post): add links field

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,17 +1,47 @@
-import { makeSource, defineDocumentType } from '@contentlayer/source-files';
+import {
+  makeSource,
+  defineDocumentType,
+  defineNestedType,
+} from '@contentlayer/source-files';
+
+const DescribedLink = defineNestedType(() => ({
+  name: 'DescribedLink',
+  fields: {
+    description: { type: 'string' },
+    href: { type: 'string' },
+  },
+}));
+
+const Links = defineNestedType(() => ({
+  name: 'Links',
+  fields: {
+    repo: { type: 'list', of: { type: 'string' } },
+    target: { type: 'list', of: { type: 'string' } },
+    assets: { type: 'list', of: DescribedLink },
+  },
+}));
 
 const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: '**/*.md',
+  filePathPattern: '*.md',
   contentType: 'markdown',
   fields: {
     title: { type: 'list', of: { type: 'string' } },
     keywords: { type: 'list', of: { type: 'string' } },
     description: { type: 'markdown' },
+    links: { type: 'nested', of: Links },
   },
 }));
 
 export default makeSource({
   contentDirPath: 'content',
   documentTypes: [Post],
+  onSuccess: async (importData) => {
+    const { allDocuments } = await importData();
+    console.log({
+      allDocuments: allDocuments.map(
+        (document) => document._raw.sourceFilePath
+      ),
+    });
+  },
 });
