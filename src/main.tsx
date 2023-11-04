4426f210a52d86b2e0e758219ac5f9200b110633
chrjl <73675826+chrjl@users.noreply.github.com>
2023-11-03 21:41:46 -0700

    build: import fonts and set tailwind family defaults

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import '@fontsource-variable/inter';
+import '@fontsource/arimo/latin-400.css';
+import '@fontsource/arimo/latin-400-italic.css';
+import '@fontsource/arimo/latin-700.css';
+import '@fontsource/cousine/latin-400.css';
+import '@fontsource/cousine/latin-700.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter Variable'],
+        content: ['Arimo'],
+        mono: ['Cousine'],
+      },
+    },
   },
   plugins: [],
 };
