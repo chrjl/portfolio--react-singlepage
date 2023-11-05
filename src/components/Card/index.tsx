0b1ab24b2072fe3c6c48ce7ba113399665335faa
chrjl <73675826+chrjl@users.noreply.github.com>
2023-11-05 07:45:17 -0800

    feat(Card): add keywords prop, CardKeywords component
    null guard included in parent component (Card)

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,20 @@
 import CardTitle from './CardTitle';
+import CardKeywords from './CardKeywords';
 
 interface CardProps {
   title: string | string[];
+  keywords?: string[];
 }
 
 export default function Card(props: CardProps) {
-  const { title } = props;
+  const { title, keywords } = props;
 
   return (
     <div className="group h-full border-2 border-solid border-black p-4 text-lg hover:bg-gray-200">
       <CardTitle title={title} />
+      <div className="group text-center font-semibold group-hover:font-bold sm:text-left">
+        {keywords && <CardKeywords keywords={keywords} />}
+      </div>
     </div>
   );
 }
