43852970e9173273f4e75abb5d3116eae1173f7c
chrjl <73675826+chrjl@users.noreply.github.com>
2023-11-05 23:24:45 -0800

    style(App): add brand mark SVGs, adjust spacing
    Brand marks added to page header: Node.js, React, TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import githubMark from './assets/github-mark.svg';
 import faLocationDot from './assets/fa-location-dot.svg';
+import nodeLogo from './assets/nodejs-stacked-dark.svg';
+import reactLogo from './assets/react.svg';
+import tsLogo from './assets/ts-logo-128.svg';
 
 import Card from './components/Card';
 
@@ -9,14 +12,27 @@ function App() {
   return (
     <div className="container mx-auto my-8 max-w-screen-sm whitespace-pre-line font-sans">
       <header className="my-8 text-center">
-        <h1 className="mb-4 text-4xl font-bold">Christopher Lee</h1>
-        <h2 className="text-xl font-semibold">
-          JavaScript Developer (Node.js, React)
-          <div className="text-base font-medium">
+        <h1 className="text-4xl font-extrabold">Christopher Lee</h1>
+
+        <h2 className="mt-4 text-xl font-bold">
+          JavaScript Developer
+          <span className="ml-4 inline-block [&>a]:ml-1.5 [&_img]:inline [&_img]:h-6">
+            <a href="https://nodejs.org/about" target="_blank">
+              <img src={nodeLogo} />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} />
+            </a>
+            <a href="https://typescriptlang.org" target="_blank">
+              <img src={tsLogo} />
+            </a>
+          </span>
+          <div className="mt-1 text-base font-medium">
             AWS Certified Solutions Architect Associate
           </div>
         </h2>
-        <h3 className="mt-2 text-sm [&>*]:align-middle">
+
+        <h3 className="mt-4 text-sm [&>*]:align-middle">
           <img height="100%" className="mr-1 inline" src={faLocationDot} />
           <span>Los Angeles, California</span>
           <span className="px-4">|</span>
@@ -27,7 +43,7 @@ function App() {
         </h3>
       </header>
 
-      <ul>
+      <ul className="mt-16">
         {allPosts.length &&
           allPosts.map((post) => (
             <li key={post._id} className="m-8">
