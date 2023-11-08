cddf7868d53355374ccd28a189bf1447c27e75de
chrjl <73675826+chrjl@users.noreply.github.com>
2023-11-07 20:55:02 -0800

    feat(Card): add description section
    Render the description HTML provided by Contentlayer (Markdown type)
    - Sanitize the HTML using isomorphic-dompurify
    - Convert the HTML into React components using react-html-parser
    - Wrap the components in a content class

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,8 @@
 import { sanitize } from 'isomorphic-dompurify';
 import parse from 'html-react-parser';
 
+import { Markdown } from '../../../.contentlayer/generated/types';
+
 import CardTitle from './CardTitle';
 import CardKeywords from './CardKeywords';
 import CardLinks, { LinksType } from './CardLinks';
@@ -9,10 +11,11 @@ interface CardProps {
   title: string | string[];
   keywords?: string[];
   links?: LinksType;
+  description?: Markdown;
 }
 
 export default function Card(props: CardProps) {
-  const { title, keywords, links } = props;
+  const { title, keywords, description, links } = props;
 
   return (
     <div className="group h-full border-2 border-solid border-black p-4 text-lg hover:bg-gray-200">
@@ -20,6 +23,12 @@ export default function Card(props: CardProps) {
         <CardTitle title={title} />
         {keywords && <CardKeywords keywords={keywords} />}
       </div>
+
+      {description && (
+        <div className="content mt-8 text-sm">
+          {parse(sanitize(description.html))}
+        </div>
+      )}
 
       {links && (
         <div className="mt-8 group-hover:font-bold">
